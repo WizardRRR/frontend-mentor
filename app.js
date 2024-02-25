aa9215a1d5d8f5cd3a9f344a99e5c689b5d4420f
Royer Rutti Ramos <ruttiramosroyer@gmail.com>
2024-02-24 23:05:36 -0500

    feat(home): :sparkles: add-page-home

---
+++ app.js
@@ -0,0 +1,7 @@
+// Estilos para la consola
+const estiloNormal = 'color: black; background-color: #e0e0e0; padding: 5px; border-radius: 3px;';
+const estiloError = 'color: white; background-color: #ff0000; padding: 5px; border-radius: 3px;';
+
+// Mensajes personalizados
+console.log('%cQue miras causita!? 🤨', estiloNormal);
+console.error('%cSi lees esto tienes que darle una estrellita al repo o te ira muy mal en la vida', estiloError);
